0ce2971493ee01c571ff153b301e360cf04df664
acsnorbert <acsnorbert@turr.hu>
2025-09-14 16:39:22 +0200

    Backend fixalasa

--- server.js
+++ server.js
@@ -1,135 +1,169 @@
-const express = require('express');
+const express = require('express')
 const fs = require('fs');
 const path = require('path');
 var cors = require('cors')
- 
-const app = express();
- 
+const { json } = require('stream/consumers');
+const app = express()
+
 // Middleware-ek
 app.use(cors())
-app.use(express.json()); // json formatum megkövetelése
-app.use(express.urlencoded({extended: true})); // req body-n keresztül átmenjenek az adatok
- 
-let users = [];
-const USERS_FILE = path.join(__dirname, 'users.json');
-loadUsers();
- 
+app.use(express.json()) //json formátum megkövetelése
+app.use(express.urlencoded({extended: true})); //req body-n keresztül átmenjenek az adatok
+
+
+let users = []
+const USERS_FILE = path.join(__dirname, 'user.json')
+
+loadUsers()
 // ENDPOINTS
- 
+
 app.get('/', (req, res) => {
-  res.send({msg:'Backend made by - 13.a Bajai SZC Türr István Technikum szoftverfejlesztő'})
-});
- 
+  res.send({msg:'Backend API by Bajai SZC Türr István Technikum - 13.A Szoftverfejlesztő '})
+})
 
 // GET all users
-app.get("/users", (req, res) => {
-    res.send((users));
-})
- 
- 
-// GET one user by id
-app.get("/users/:id", (req, res) => {
-    let id = req.params.id;
-    let idx = users.findIndex(user => user.id == id);
-    if (idx > -1) {
-        return res.send(users[idx]);
-    }
-    return res.send({msg:'Nincs ilyen azonosítójú felhasználó!'});
-});
- 
-// POST new user
-app.post('/users', (req, res) => {
-    let data = req.body;
-    if (IsEmailExists(data.email)){
-        return res.status(400).send({msg: 'Ez az email cím már regisztrálva van!'})
-    }
-    data.id = getNextID();
-    users.push(data);
-    saveUsers();
-    res.send({msg:'A felhasználó sikeresen regisztrálva'});
+
+app.get('/users', (req, res)=>{
+    res.send(users)
 });
 
-//post check user login
-app.post('/users/login', (req,res) =>{
-    let {email,password} = req.body
+// GET one user by id
+
+app.get('/users/:id',(req,res)=>{
+    let id = Number(req.params.id)
+    let idx = users.findIndex(user => Number(user.id) === id)
+    if(idx >-1){
+        return res.send(users[idx])
+    }
+    return res.status(400).send({msg:"Nincs ilyen azonosítójú felhasználó!"})
+})
+
+
+// POST new user
+app.post('/users', (req,res)=>{
+ let data = req.body;
+ if(isEmailExist(data.email)){
+    return res.status(400).send({msg:"bademail"})
+ }
+ data.id = getNextId();
+ users.push(data)
+ res.send({msg: "Sikeres regisztráció!"})
+ saveUsers()
+});
+
+//POST user login
+
+app.post('/users/login', (req, res) => {
+    let {email, password} = req.body;
     let loggeduser = {}
-    users.forEach(user => {
-        if (user.email == email && user.password == password){
+    users.forEach(user=> {
+        if(user.email == email && user.password == password){
             loggeduser = user
             return
         }
-        res.send(loggeduser)
+
     })
+    res.send(loggeduser)
+})
+// DELETE user
+app.delete('/users/:id', (req,res)=>{
+    let id = Number(req.params.id)
+    let idx = users.findIndex(user => Number(user.id) === id)
+    if(idx >-1){
+        users.splice(idx,1)
+        saveUsers()
+        return res.send({msg:"A felhasználó törölve."})
+    }
+    return res.status(400).send({msg:"Nincs ilyen azonosítójú felhasználó!"})
 })
 
-
-// DELETE user by id
-app.delete('/users/:id', (req, res) => {
-    let id = req.params.id;
-    let idx = users.findIndex(user => user.id == id);
-    if (idx > -1) {
-        users.splice(idx, 1);
-        saveUsers();
-        return res.send("A felhasználó törölve.");
-    }
-    return res.send({msg:'Nincs ilyen azonosítójú felhasználó!'})
-});
- 
 // UPDATE user by id
-app.patch('/users/:id', (req, res) => {
-    let id = req.params.id;
-    let idx = users.findIndex(user => user.id == id);
-    let data = req.body;
-    if (idx > -1) {
-        users[idx] = data;
-        users[idx].id = Number(id);
-        saveUsers();
-        res.send({msg:'A felhasználó módosítva'})
-    }
-});
- 
-app.listen(3000);
 
-function getNextID() {
+app.patch('/users/:id', (req, res) => {
+    let id = Number(req.params.id)
+    let data = req.body
+    let idx = users.findIndex(user => Number(user.id) === id)
+    if (idx > -1) {
+        if (data.email && data.email != users[idx].email) {
+            let exists = users.some(user => user.email === data.email && Number(user.id) !== id)
+            if (exists) {
+                return res.status(400).send({ msg: "Ez az email cím már foglalt!" })
+            }
+            users[idx].email = data.email
+        }
+        if (data.name) users[idx].name = data.name
+        saveUsers()
+        return res.send({ msg: "A felhasználó módosítva.", user: users[idx] })
+    }
+    return res.status(400).send({ msg: "Nincs ilyen azonosítójú felhasználó!" })
+})
+
+//UPDATE password
+app.patch('/users/changepass/:id', (req, res) => {
+    let id = Number(req.params.id)
+    let data = req.body
+    let idx = users.findIndex(user => Number(user.id) === id)
+    if (idx > -1) {
+        if (data.oldpass && data.newpass) {
+            if (data.oldpass != users[idx].password) {
+                return res.status(400).send({ msg: "A régi jelszó nem megfelelő!" })
+            }
+            users[idx].password = data.newpass
+            saveUsers()
+            return res.send({ msg: "A jelszó módosítva.",user : users[idx] })
+        }
+        return res.status(400).send({ msg: "Nincsenek meg a szükséges adatok!" })
+    }
+    return res.status(400).send({ msg: "Nincs ilyen azonosítójú felhasználó!" })
+})
+
+app.listen(3000)
+
+function getNextId(){
     let nextID = 1;
+
     if (users.length == 0){
         return nextID
-    } 
- 
+    }
+    
     let maxIndex = 0
     for (let i = 0; i < users.length; i++) {
-        if (users[i].id > users[maxIndex].id) {
-            maxIndex = i;
+        if(users[i].id > users[maxIndex].id){
+            maxIndex = i
         }
+        
     }
     return users[maxIndex].id + 1
 }
 
-function loadUsers() {
-    if (fs.existsSync(USERS_FILE)) {
-        const raw = fs.readFileSync(USERS_FILE);
-        try {
-            users = JSON.parse(raw);
-        } catch (error) {
-            console.log('Hiba az adatok beolvasása során', error)
-            users = [];
+function loadUsers(){
+    if(fs.existsSync(USERS_FILE)){
+        const raw = fs.readFileSync(USERS_FILE)
+        try{
+            users = JSON.parse(raw)
         }
-    } else {
-        saveUsers();
+        catch(err){
+            console.log("Hiba az adatok beolvasása közben!", err)
+            users = [];
+
+        }
     }
-}
- 
-function saveUsers() {
-    fs.writeFileSync(USERS_FILE, JSON.stringify(users));
+    else{
+        saveUsers()
+    }
+   
 }
 
-function IsEmailExists(email){
-    let exists= false;
-    users.forEach(user => {
-        if(user.email==email){
-            exists=true
+function saveUsers(){
+    fs.writeFileSync(USERS_FILE,JSON.stringify(users))
+}
+function isEmailExist(email){
+    let exists = false
+    users.forEach(user=> {
+        if(user.email == email){
+            exists = true
             return
         }
-    });
+    })
     return exists
 }
