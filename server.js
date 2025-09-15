29379e2c2ed9eec31375c0e75595f6ec7c05f74a
acsnorbika <acsnorbert@turr.hu>
2025-09-15 12:24:27 +0200

    megy a munka

--- server.js
+++ server.js
@@ -12,14 +12,19 @@ app.use(express.urlencoded({extended: true})); //req body-n keresztül átmenjen
 
 
 let users = []
+let steps = []
 const USERS_FILE = path.join(__dirname, 'user.json')
+const STEPS_FILE = path.join(__dirname, 'steps.json')
 
 loadUsers()
+loadSteps()
 // ENDPOINTS
 
 app.get('/', (req, res) => {
   res.send({msg:'Backend API by Bajai SZC Türr István Technikum - 13.A Szoftverfejlesztő '})
 })
+
+// ----------------------- USERS --------------------
 
 // GET all users
 
@@ -45,7 +50,7 @@ app.post('/users', (req,res)=>{
  if(isEmailExist(data.email)){
     return res.status(400).send({msg:"bademail"})
  }
- data.id = getNextId();
+ data.id = getNextId('users');
  users.push(data)
  res.send({msg: "Sikeres regisztráció!"})
  saveUsers()
@@ -117,25 +122,116 @@ app.patch('/users/changepass/:id', (req, res) => {
     return res.status(400).send({ msg: "Nincs ilyen azonosítójú felhasználó!" })
 })
 
+// --------------- STEPS ---------------------
+
+// GET all steps by userId
+app.get('/steps/user/:uid', (req, res) => {
+    let uid = Number(req.params.uid);
+    let matchSteps = [];
+    steps.forEach(step => {
+        if (step.uid === uid) {
+            matchSteps.push(step);
+        }
+    });
+    res.send(matchSteps);
+});
+ 
+ 
+// GET one step by id
+ 
+app.get('/steps/:id', (req, res) => {
+    let id = req.params.id;
+    let idx = steps.findIndex(step => step.id == id);
+    if (idx > -1) {
+        return res.send(steps[idx]);
+    }
+    return res.status(400).send({msg: "Nincs ilyen azonosítójú lépésszám!"});
+});
+ 
+// POST new step by uid
+ 
+app.post('/steps/upload/:uid', (req, res) => {
+    let data = req.body;
+    let uid = Number(req.params.uid);
+    steps.push(data);
+    data.id = getNextStepID();
+    data.uid = uid;
+    saveSteps();
+    res.send({msg: 'A lépés felvéve!'});
+})
+ 
+// PATCH step by id
+app.patch('/steps/:id', (req, res) => {
+    let data = req.body;
+    let id = Number(req.params.id);
+ 
+    let newDate = data.newDate;
+    let newCount = Number(data.newCount);
+ 
+    steps.forEach(step => {
+        if (step.id === id) {
+            step.date = newDate;
+            step.count = newCount;
+        }
+    });
+    saveSteps();
+    res.send({msg: 'Sikeres módosítás'})
+});
+ 
+ 
+// DELETE step by id
+app.delete('/steps/:id', (req, res) => {
+    let id = Number(req.params.id);
+    let idx = steps.findIndex(step => step.id == id);
+ 
+    if (idx > -1) {
+        steps.splice(idx, 1);
+        saveSteps();
+        return res.status(200).send({msg: 'Sikeres törlés'})
+    }
+    return res.status(400).send({msg: "Nincs ilyen azonosítójú lépésszám!"});
+})
+ 
+ 
+// DELETE all steps by userId
+app.delete('/steps/users/:uid', (req, res) => {
+    let uid = Number(req.params.uid);
+ 
+    for (let i = 0; i < steps.length; i++) {
+        if (steps[i].uid == uid) {
+            steps.splice(i, 1);
+        }
+    }
+});
+
+
+
+
 app.listen(3000)
 
 function getNextId(){
     let nextID = 1;
 
-    if (users.length == 0){
+    if (table.length == 0){
         return nextID
     }
     
     let maxIndex = 0
-    for (let i = 0; i < users.length; i++) {
-        if(users[i].id > users[maxIndex].id){
+    for (let i = 0; i < table.length; i++) {
+        if(table[i].id > table[maxIndex].id){
             maxIndex = i
         }
         
     }
     return users[maxIndex].id + 1
 }
-
+function getNextStepID() {
+    const maxId = steps.reduce((max, u) => {
+        const id = Number(u?.id);
+        return Number.isFinite(id) && id > max ? id : max;
+    }, 0);
+    return maxId + 1;
+}
 function loadUsers(){
     if(fs.existsSync(USERS_FILE)){
         const raw = fs.readFileSync(USERS_FILE)
@@ -145,6 +241,27 @@ function loadUsers(){
         catch(err){
             console.log("Hiba az adatok beolvasása közben!", err)
             users = [];
+
+        }
+    }
+    else{
+        saveUsers()
+    }
+   
+}
+function saveSteps() {
+    fs.writeFileSync(STEPS_FILE, JSON.stringify(steps));
+}
+
+function loadSteps(){
+    if(fs.existsSync(STEPS_FILE)){
+        const raw = fs.readFileSync(USERS_FILE)
+        try{
+            steps = JSON.parse(raw)
+        }
+        catch(err){
+            console.log("Hiba az adatok beolvasása közben!", err)
+            steps = [];
 
         }
     }
